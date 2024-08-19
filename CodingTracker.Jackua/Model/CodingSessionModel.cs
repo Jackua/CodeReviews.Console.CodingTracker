d0e31f36561259cebf789ef742a60716936a5f05
Jackua <kuangja@oregonstate.edu>
2024-08-18 20:50:32 -0400

    Created Model

---
+++ CodingTracker.Jackua/Model/CodingSessionModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingTracker.Jackua.Model;
+
+internal class CodingSessionModel
+{
+    internal int Id { get; init; }
+    internal DateTime StartTime { get; init; }
+    internal DateTime EndTime { get; init; }
+    internal TimeSpan Duration { get; }
+
+    internal CodingSessionModel(int id, DateTime startTime, DateTime endTime)
+    {
+        Id = id;
+        StartTime = startTime;
+        EndTime = endTime;
+        Duration =StartTime.Subtract(EndTime);
+    }
+}
