5c21d804f8fc4a4369ae3ea29134391ca649945a
Jackua <kuangja@oregonstate.edu>
2024-08-28 13:37:29 -0400

    Updated with Duration property

--- CodingTracker.Jackua/Model/CodingSessionModel.cs
+++ CodingTracker.Jackua/Model/CodingSessionModel.cs
@@ -3,15 +3,15 @@
 internal class CodingSessionModel
 {
     internal int Id { get; init; }
-    internal DateTime StartTime { get; init; }
-    internal DateTime EndTime { get; init; }
+    internal DateTime StartDateTime { get; init; }
+    internal DateTime EndDateTime { get; init; }
     internal TimeSpan Duration { get; }
 
     internal CodingSessionModel(int id, DateTime startTime, DateTime endTime)
     {
         Id = id;
-        StartTime = startTime;
-        EndTime = endTime;
-        Duration =StartTime.Subtract(EndTime);
+        StartDateTime = startTime;
+        EndDateTime = endTime;
+        Duration = StartDateTime.Subtract(EndDateTime);
     }
 }
