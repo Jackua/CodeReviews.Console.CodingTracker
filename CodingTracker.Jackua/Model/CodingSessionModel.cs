0aefa8d008fa60482ed0abe895c7a4402db1b128
Jackua <kuangja@oregonstate.edu>
2024-09-01 00:44:52 -0400

    Added id

--- CodingTracker.Jackua/Model/CodingSessionModel.cs
+++ CodingTracker.Jackua/Model/CodingSessionModel.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
-
-namespace CodingTracker.Jackua.Model;
+﻿namespace CodingTracker.Jackua.Model;
 
 internal class CodingSessionModel
 {
+    internal int Id { get; set; } = -1;
     internal DateTime StartDateTime { get; init; }
     internal DateTime EndDateTime { get; init; }
     internal TimeSpan Duration { get { return EndDateTime.Subtract(StartDateTime); } }
 
-    internal void Deconstruct(out string startDateTime, out string endDateTime, out string duration)
+    internal void Deconstruct(out int id, out string startDateTime, out string endDateTime, out string duration)
     {
+        id = this.Id;
         startDateTime = this.StartDateTime.ToString();
         endDateTime = this.EndDateTime.ToString();
         duration = this.Duration.ToString(string.Format("G"));
